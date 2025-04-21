da75b06cf5e7da22fe69b7e743c3daa807bddb24
Soumayabenahmed <soumaya.benahmed@kelibia.r-iset.tn>
2025-04-21 19:47:02 +0100

    initial

---
+++ src/main/java/com/rendezvous/management/rendezvousmanagement/RendezvousManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rendezvous.management.rendezvousmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendezvousManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RendezvousManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rendezvous/management/rendezvousmanagement/RendezvousManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rendezvous.management.rendezvousmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendezvousManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
